f034d934c558480ff32b059adfc5c287e8b6a9b5
Varshinii23 <129053270+Varshinii23@users.noreply.github.com>
2024-09-13 13:12:05 +0530

    Add files via upload

---
+++ Task8-java/InstanceVariable.java
@@ -0,0 +1,23 @@
+package com.index;
+
+import java.util.Scanner;
+
+public class InstanceVariable {
+
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner scan=new Scanner(System.in);
+		System.out.println("what is your name");
+		String name=scan.nextLine();
+		System.out.println("what is your age");
+		int age=scan.nextInt();
+		System.out.println("what is your cgpa");
+		float cgpa=scan.nextFloat();
+		
+		
+
+	}
+
+}
