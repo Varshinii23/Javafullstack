70df61cbfec7689be35313fbb811d5db8d346fda
Varshinii23 <129053270+Varshinii23@users.noreply.github.com>
2024-09-19 09:03:58 +0530

    Add files via upload

---
+++ Task10-java/Age.java
@@ -0,0 +1,28 @@
+package com.index;
+
+public class Age {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String name="varsh";
+		int id=003;
+		double sal=35000;
+		
+		System.out.println("the employee name is " +name);
+		System.out.println("the employee id is " +id);
+		System.out.println("the employee salary is " +sal);
+		
+		int age=91;
+		if(age>90)
+		{
+			System.out.println("you are not eligible to work in a company");
+		}
+		else
+		{
+			System.out.println("you are eligible to work in a company");
+		}
+
+
+	}
+
+}
---
+++ Task10-java/Number.java
@@ -0,0 +1,25 @@
+package com.index;
+
+public class Number {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+			int number=-4;
+			if(number >0) {
+				System.out.println("this is possitive integer");
+			}
+			else if(number<0)
+			{
+				System.out.println("this is negative integer");
+			}
+			else
+			{
+				System.out.println("this is zero");
+			}
+
+
+	}
+
+}
